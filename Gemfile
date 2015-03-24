886aeb459c5c93815fb7bbbfdcddeeeee77e7478
Tomoyuki Tarumi <tarumitomoyuki@gmail.com>
2015-03-24 18:02:26 +0900

    change gulp task

--- Gemfile
+++
@@ -1,5 +0,0 @@
-# A sample Gemfile
-source "https://rubygems.org"
-
-gem 'compass', '0.12.6'
-gem 'bourbon', '3.2.3'
---
+++ Gulpfile.js
@@ -0,0 +1,48 @@
+var del      = require('del')
+  , gulp     = require('gulp')
+  , gsass    = require('gulp-sass')
+  , gkss     = require('gulp-kss')
+  , gconcat  = require('gulp-concat')
+  , runseq   = require('run-sequence')
+  , bourbon  = require('node-bourbon');
+;
+
+bourbon.with('src/*.scss');
+
+gulp.task('clean', function(cb) {
+  return del([
+    'dist/',
+    'doc/'
+  ], cb);
+});
+
+gulp.task('kss', function() {
+  return gulp.src('src/*.scss')
+    .pipe(gkss({
+      overview:          'src/styleguide.md',
+      templateDirectory: 'helpers/kss_template'
+    }))
+    .pipe(gulp.dest('doc/'))
+  ;
+});
+
+gulp.task('kss-concat', function() {
+  return gulp.src('src/*.scss')
+    .pipe(gsass({
+      includePaths: bourbon.includePaths,
+      outputStyle: 'compressed'
+    }))
+    .pipe(gconcat('public/style.css'))
+    .pipe(gulp.dest('doc/'))
+  ;
+});
+
+gulp.task('kss-copy', function() {
+  return gulp.src('helpers/assets/*')
+    .pipe(gulp.dest('doc/assets/'))
+  ;
+});
+
+gulp.task('default', function() {
+  runseq('clean', 'kss-concat', 'kss-copy', 'kss')
+});
